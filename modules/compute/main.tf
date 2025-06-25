5d30ed012c68a217bee00fe1a0268c16c0028936
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-25 15:29:54 +0200

    Terraform Transformation project

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -5,15 +5,6 @@ resource "aws_instance" "docker_host" {
   key_name                    = var.key_name
   associate_public_ip_address = true
 
-  user_data = <<-EOF2
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              docker run -d --name ssh-clean repping/ssh-clean:1.4.4
-              EOF2
-
   tags = {
     Name = "${var.project_name}-docker-host"
   }
