be9da1cf1b34040c5607d2073088c4e60aacc03a
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-25 16:15:58 +0200

    Terraform Transformation project

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,12 +1,29 @@
-resource "aws_security_group" "allow_all" {
-  name        = "${var.project_name}-allow-all"
-  description = "Allow all inbound traffic"
+resource "aws_security_group" "allow_essential" {
+  name        = "${var.project_name}-sg"
+  description = "Allow essential inbound traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
