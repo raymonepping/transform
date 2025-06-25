4dce9f78e486c0cd77d9ab0e50e3b407e9cdb082
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-25 12:53:27 +0200

    Terraform Transformation project

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "example" {
   ami                    = "ami-06dd92ecc74fdfb36"  # Ubuntu 22.04 for eu-north-1
   instance_type          = "t3.micro"
-  subnet_id              = module.network.aws_subnet_id
   associate_public_ip_address = true
 
   tags = {
