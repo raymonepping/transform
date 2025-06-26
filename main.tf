2bb543b15922258b455c9e64726bc5ed9503b88e
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-26 15:08:39 +0200

    Terraform Docker Transformation project

--- main.tf
+++ main.tf
@@ -11,15 +11,3 @@ provider "docker" {
   host = "unix:///Users/raymon.epping/.docker/run/docker.sock"
 }
 
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
-
-resource "docker_container" "nginx" {
-  name  = "demo-nginx"
-  image = docker_image.nginx.latest
-  ports {
-    internal = 80
-    external = 8080
-  }
-}
