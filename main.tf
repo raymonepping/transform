505326f1435689a8ff03e9f4d7a5dad681527bfe
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-26 15:14:14 +0200

    Terraform Docker Transformation project

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ provider "docker" {
   host = "unix:///Users/raymon.epping/.docker/run/docker.sock"
 }
 
+resource "docker_image" "hello" {
+  name = "hello-world:latest"
+}
+
+resource "docker_container" "hello" {
+  name  = "hello-from-terraform"
+  image = docker_image.hello.latest
+  must_run = false
+  rm       = true
+}
