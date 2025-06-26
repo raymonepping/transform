d6feaf440d0a89637dcfea8f8d40abc53d6f7e9b
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-26 17:03:08 +0200

    Terraform Docker Transformation project

--- modules/compute/mysql.tf
+++ modules/compute/mysql.tf
@@ -1,6 +1,10 @@
 resource "docker_container" "mysql" {
   name  = "mysql"
   image = var.mysql_image
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=rootpassword"
+  ]
   ports {
     internal = 3306
     external = 3306
