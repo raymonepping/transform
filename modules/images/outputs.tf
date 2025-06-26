2ae55df3cbe6f3c9e8c0a5839503cf718cdadd72
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-26 16:57:20 +0200

    Terraform Docker Transformation project

--- modules/images/outputs.tf
+++ modules/images/outputs.tf
@@ -1,9 +1,6 @@
 output "ssh_clean_image" {
   value = docker_image.ssh-clean.name
 }
-output "ssh_clean_image" {
-  value = docker_image.ssh-clean.name
-}
 output "mysql_image" {
   value = docker_image.mysql.name
 }
