6e37a73b85bcb7e597c2d2aa68c6d7ca504f8f78
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-26 13:39:13 +0200

    Terraform Docker Transformation project

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
 module "network" {
   source = "./modules/network"
+  providers = {
+    docker = docker
+  }
 }
 
 module "storage" {
   source = "./modules/storage"
+  providers = {
+    docker = docker
+  }
 }
 
 module "compute" {
   source = "./modules/compute"
+  providers = {
+    docker = docker
+  }
 }
